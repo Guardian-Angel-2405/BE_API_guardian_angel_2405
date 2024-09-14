8575de946f091dc46a32dc1decf3b3d196e058cc
Evan Davis <evandavis427@gmail.com>
2024-09-14 13:30:48 -0600

    parsed json

--- spec/requests_spec.rb
+++ spec/requests_spec.rb
@@ -2,15 +2,8 @@
 require 'spec_helper'
 require 'json'
 
-
 RSpec.describe 'Guardian Angel API' do
   include Rack::Test::Methods
-
-  # Helper method to load fixture files
-  def load_fixture(file_name)
-    file_path = File.join(File.dirname(__FILE__), 'fixtures', file_name)
-    JSON.parse(File.read(file_path))
-  end
 
   before(:each) do
     # Stub the OAuth token request
@@ -27,7 +20,6 @@ RSpec.describe 'Guardian Angel API' do
         'User-Agent' => 'Faraday v2.11.0'
       }
     ).to_return(status: 200, body: '{"access_token":"fake_access_token"}', headers: {})
-
 
     # Stub the /countries API endpoint
     stub_request(:get, "https://api.throughlinecare.com/v1/countries")
@@ -49,60 +41,79 @@ RSpec.describe 'Guardian Angel API' do
 
   # Test the /countries endpoint
   describe 'GET /countries' do
-    it 'returns a list of countries' do
+    it 'returns a list of countries with correct fields' do
       get '/countries'
 
-      # puts last_response.body if last_response.status != 200
-
       expect(last_response.status).to eq(200)
+      
       response_body = JSON.parse(last_response.body)
+      
+      # Parse the stringified 'countries' field
+      countries = JSON.parse(response_body)['countries']
 
-      # Compare the response with the loaded fixture
-      # expected_data = load_fixture('countries.json')['countries']
-      # expect(response_body['countries']).to eq(expected_data)
+      expect(countries).to be_an(Array)
+
+      first_country = countries.first
+      expect(first_country['name']).to eq('New Zealand')
+      expect(first_country['code']).to eq('NZ')
     end
   end
 
   # Test the /topics endpoint
   describe 'GET /topics' do
-    it 'returns a list of topics' do
+    it 'returns a list of topics with correct fields' do
       get '/topics'
 
       expect(last_response.status).to eq(200)
+      
       response_body = JSON.parse(last_response.body)
+      
+      # Parse the stringified 'topics' field
+      topics = JSON.parse(response_body)['topics']
 
-      # Compare the response with the loaded fixture
-      # expected_data = load_fixture('topics.json')['topics']
-      # expect(response_body['topics']).to eq(expected_data)
+      expect(topics).to be_an(Array)
+
+      first_topic = topics.first
+      expect(first_topic['name']).to eq('Abuse & domestic violence')
+      expect(first_topic['code']).to eq('abuse-domestic-violence')
     end
   end
 
   # Test the /helplines endpoint
   describe 'GET /helplines' do
-    it 'returns a list of helplines' do
+    it 'returns a list of helplines with correct fields' do
       get '/helplines?country_code=us&limit=5'
 
       expect(last_response.status).to eq(200)
-      response_body = JSON.parse(last_response.body)
 
-      # Compare the response with the loaded fixture
-      # expected_data = load_fixture('helplines.json')['helplines']
-      # expect(response_body['helplines']).to eq(expected_data)
+      response_body = JSON.parse(last_response.body)
+      
+      # Parse the stringified 'helplines' field
+      helplines = JSON.parse(response_body)['helplines']
+
+      expect(helplines).to be_an(Array)
+
+      first_helpline = helplines.first
+      expect(first_helpline['name']).to eq('988 Suicide & Crisis Lifeline')
+      expect(first_helpline['phoneNumber']).to eq('988')
     end
   end
 
   # Test the /helplines/:id endpoint
   describe 'GET /helplines/:id' do
-    it 'returns details of a specific helpline' do
+    it 'returns details of a specific helpline with correct fields' do
       valid_helpline_id = 'c8e47108-3f87-4311-ab8f-7a3adf01ba06'
       get "/helplines/#{valid_helpline_id}"
 
       expect(last_response.status).to eq(200)
       response_body = JSON.parse(last_response.body)
 
-      # Load expected data from the fixture
-      # expected_helpline = load_fixture('helpline_details.json')['helpline']
-      # expect(response_body['helpline']).to eq(expected_helpline)
+      # Parse the stringified 'helpline' field
+      helpline = JSON.parse(response_body)['helpline']
+
+      expect(helpline['name']).to eq('988 Suicide & Crisis Lifeline')
+      expect(helpline['phoneNumber']).to eq('988')
+      expect(helpline['description']).to include('suicide prevention and crisis intervention service')
     end
   end
 end
